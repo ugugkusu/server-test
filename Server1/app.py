c9b2dec63403cc437326a6fd0811539c65d283d7
Uğur Güneş <ugugkusu@gmail.com>
2024-07-24 18:29:06 +0300

    test added

---
+++ Server1/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    client_ip = request.remote_addr
+    return "Server 1'den selamlar. Burası 'index' sayfası. Sen bu {} IP Adresine sahipsin".format(client_ip)
+
+@app.route('/example-endpoint')
+def example_endpoint():
+    client_ip = request.remote_addr
+    return "Server 1'den selamlar. Burası 'example-endpoint' sayfası. Sen bu {} IP Adresine sahipsin".format(client_ip)
+
+@app.route("/health-check")
+def health_check():
+    client_ip = request.remote_addr
+    return "OK {}".format(client_ip), 200
